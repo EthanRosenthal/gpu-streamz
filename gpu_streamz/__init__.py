640a409c17b7e8cecd67fd99db0bec99ae679927
EthanRosenthal <ethanrosenthal@gmail.com>
2018-01-07 16:06:46 -0500

    refactor nvidia-smi parsing

--- gpu_streamz/__init__.py
+++ gpu_streamz/__init__.py
@@ -1,3 +1,4 @@
+from io import StringIO
 from subprocess import check_output
 from time import time
 
@@ -8,51 +9,30 @@ from tornado.ioloop import IOLoop
 from tornado import gen
 
 
-def _parse_value(line, unit):
-    return line.split(':')[-1].rstrip(unit).strip()
-
-
 def _nvidia_smi():
     """
     Determine GPU stats using nvidia-smi command line tool.
 
-    This is an ungodly hack.
+    Props to Ryan Vilim (@rvilim) for pointing out how to better use the 
+    nvidia-smi command line arguments.
     """
-    status = check_output(['nvidia-smi', '-q'])
-    status = status.decode('utf-8').split('\n')
-    util_found = False
-    memory_found = False
-    for i in range(len(status)):
-        line = status[i]
-        if line.lstrip().startswith('Utilization'):
-            for j in range(i + 1, len(status)):
-                if util_found:
-                    break
-                sub_line = status[j]
-                if sub_line.lstrip().startswith('Gpu'):
-                    # '        Gpu                         : 9 %'
-                    util = int(_parse_value(sub_line, '%'))
-                    util_found = True
-        if line.lstrip().startswith('FB Memory Usage'):
-            total = None
-            used = None
-            for j in range(i + 1, len(status)):
-                if total is not None and used is not None:
-                    break
-                sub_line = status[j]
-                
-                if sub_line.lstrip().startswith('Total'):
-                    total = int(_parse_value(sub_line, 'MiB'))
 
-                if sub_line.lstrip().startswith('Used'):
-                    used = int(_parse_value(sub_line, 'MiB'))
-            memory = float(used) / total * 100
-            memory_found = True
-                    
-    if not util_found or not memory_found:
-        return (-1, -1)
-            
-    return (util, memory)
+    status = check_output(['nvidia-smi', 
+                           '--query-gpu=utilization.gpu,utilization.memory', 
+                           '--format=csv'])
+    status = pd.read_csv(StringIO(status.decode('utf-8')))
+    
+    # Reformat column names.
+    # (Need the col.strip() because sometimes there are preceding spaces)
+    map_cols = {'utilization.gpu [%]': 'Utilization (%)',
+                'utilization.memory [%]': 'Memory (%)'}
+    status.columns = [map_cols[col.strip()] for col in status.columns]
+
+    # Convert to numerical data
+    for col in status.columns:
+        status[col] = status[col].apply(lambda x: int(x.rstrip('%')))
+
+    return status
 
 
 def _make_df(tup):
@@ -61,10 +41,15 @@ def _make_df(tup):
                              end=now * 1e9, 
                              freq=freq,
                              name='Time since start')
-    utilization, memory = _nvidia_smi()
-    df = pd.DataFrame({'Utilization (%)': utilization,
-                       'Memory (%)': memory},
-                      index=index)
+    df = _nvidia_smi()
+
+    if len(index) == 0:
+        # When we first call this to initialize the dataframe, the index is 
+        # empty
+        return pd.DataFrame(columns=df.columns, index=index)
+
+    df = pd.concat([df for _ in range(len(index))])
+    df.index = index
     return df
 
 
